06d94299aa41ff3151c134009aff36f9a42e264a
levinat <nat.levinzon@gmail.com>
2022-03-03 13:21:14 +0200

    Add project files.

---
+++ Controllers/ForecastAppController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ForecastApp.ForecastAppModels;
+using ForecastApp.OpenWeatherMapModels;
+using ForecastApp.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForecastApp.Controllers
+{
+    public class ForecastAppController : Controller
+    {
+        private readonly IForecastRepository _forecastRepository;
+
+        public ForecastAppController(IForecastRepository forecastAppRepo)
+        {
+            _forecastRepository = forecastAppRepo;
+        }
+
+        public IActionResult SearchCity()
+        {
+            var viewModel = new SearchCity();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult SearchCity(SearchCity model)
+        {
+            if (ModelState.IsValid) {
+                return RedirectToAction("City", "ForecastApp", new { city = model.CityName });
+            }
+            return View(model);
+        }
+
+        public IActionResult City(string city)
+        {
+            WeatherResponse weatherResponse = _forecastRepository.GetForecast(city);
+            City viewModel = new City();
+
+            if (weatherResponse != null)
+            {
+                viewModel.Name = weatherResponse.Name;
+                viewModel.Humidity = weatherResponse.Main.Humidity;
+                viewModel.Pressure = weatherResponse.Main.Pressure;
+                viewModel.Temp = weatherResponse.Main.Temp;
+                viewModel.Weather = weatherResponse.Weather[0].Main;
+                viewModel.Wind = weatherResponse.Wind.Speed;
+                viewModel.Feels_like = weatherResponse.Main.Feels_like;
+            } 
+            return View(viewModel);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ForecastApp.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ForecastApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            
+            services.AddScoped<IForecastRepository, ForecastRepository>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Shared/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=ForecastApp}/{action=SearchCity}/{id?}");
+            });
+        }
+    }
+}
